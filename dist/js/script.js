5c8f0ee05081b1e0d4d86e1dad7233b15002555a
GovalPrabawa <imadegoval726@gmail.com>
2023-07-31 23:34:22 +0800

    portfolio tailwind css

---
+++ dist/js/script.js
@@ -0,0 +1,23 @@
+//todo: Hamburger active
+const hamburger = document.querySelector('#hamburger');
+
+//todo: nav-menu
+const navMenu = document.querySelector('#nav-menu');
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+
+    //munculkan nav-menu
+    navMenu.classList.toggle('hidden');
+});
+
+// navbar fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
